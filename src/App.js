edd8b2b04eada93379d385195c8378aa294209cb
wa1ead <waliddraa2@gmail.com>
2024-11-09 17:49:25 +0100

    implementing class state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.Person = {
+      fullName: "Walid Wahid",
+      bio: "ℒ𝒊𝝂𝒆 𝓕𝒓𝒆𝒆 𝓞𝒓 𝓓𝒊𝒆 🦅",
+      profession: "Software Developer",
+      imageSrc:
+        "https://upload.wikimedia.org/wikipedia/commons/0/09/Zlatan_Ibrahimovi%C4%87_June_2018.jpg",
+    };
+    this.shows = false;
+  }
   render() {
-    return;
+    return 
   }
 }
 
